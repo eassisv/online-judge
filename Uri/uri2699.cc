aaa562cdc8ac98ecae027a47b54be1164c629c92
Éverton Assis <eassis.vieira@gmail.com>
2018-08-28 22:01:50 -0300

    *-*

--- Uri/uri2699.cc
+++ Uri/uri2699.cc
@@ -3,35 +3,34 @@ using namespace std;
 
 const int MAX = 1e3+7;
 vector< vector<int> > mm(MAX, vector<int> (MAX, -1));
+string s;
+int n;
+list< int > D;
 
-int mod(string s, int n) {
-    int r = 0;
-    cout << s.size() << endl;
-    for(int i = 0; i < s.size(); i++) {
-        int x = s[i] - '0';
-        r = r * 10 + x; 
-        r = x % n;
+int pd(int idx, int m) {
+    if(idx == (int)s.size()) return !m;
+    if(mm[idx][m] != -1) return mm[idx][m];
+    int ans, x;
+    if(s[idx] != '?') {
+        x = s[idx] - '0';
+        ans = pd(idx + 1, (m * 10 + x) % n);
+    }else {
+        x = !idx ? 1 : 0;
+        for(ans = 0; !ans && x < 10; x++) {
+            ans = pd(idx + 1, (m * 10 + x) % n);
+        }
+        x--;
     }
-    return r;
-}
-
-int pd(int idx, int m, string &s) {
-    if(idx == -1) return m;
-    
-    
+    if(ans) D.push_front(x);
+    return mm[idx][m] = ans;
 
 }
 
 int main(void) {
-    string s;
-    int n;
     cin >> s >> n;
-    cout << s << ' ' << n << endl;
-    for(int i = 0; i < (int)s.size(); i++) {
-        if(!i && s[i] == '?') s[i] = '1';
-        else if(s[i] == '?') s[i] = '0';
-    }
-    cout << pd((int)s.size() - 1, mod(s, n), s) << '\n'; 
-
+    int ans = pd(0, 0);
+    if(ans) for(auto i : D) cout << i;
+    else cout << '*';
+    cout << '\n';
     return 0;
 }
